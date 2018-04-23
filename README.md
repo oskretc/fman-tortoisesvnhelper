fae9170fe8ed3d3fbd1d3691f3287d9de662c854
Oscar Torresbaca <otorresbaca@gmail.com>
2018-04-23 14:48:26 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# TortoiseSVN Helper (Windows only) 
+[fman](https://fman.io) plugin to work with Sublime Text Editor
+
+# Installation
+1 Install using fman Install Plugin command
+
+# Usage
+* Invoke command window with Ctrl+Shift+P/Cmd+Shift+P and type SVN
+
+# Features
+* Command: **Sublime open selected**:
+  * 1 file selected, the file will be open in an existing window (same as F4 if Sublime text editor is selected)
+  * Multiple files selected, the files will be open in a new window (if folder selected, folders will also be added)
+* Command: **Sublime open current folder in new window**
+  * The current folder will be added to a new Sublime text window and any selected files will be opened
+
+# TODO
+
+* TODO: Check if quoting is working for other platforms
---
+++ tortoisesvn_helper/__init__.py
@@ -0,0 +1,115 @@
+from fman import DirectoryPaneCommand, show_alert, show_prompt, load_json, save_json, YES, NO
+from fman.fs import is_dir, exists
+from fman.url import splitscheme, as_url, as_human_readable
+from subprocess import DEVNULL, Popen
+
+import os
+import shlex
+
+
+_TORTOISEPROCPATHDEFAULT = 'C:/Program Files/TortoiseSVN/bin/TortoiseProc.exe'
+_TORTOISEPROCCONFIGFILE = 'TortoiseSVNHelper Config.json'
+_TORTOISEPROCPATH = ''
+
+settings = load_json(_TORTOISEPROCCONFIGFILE, default={'path': _TORTOISEPROCPATHDEFAULT})
+
+if settings['path'] and exists(as_url(settings['path'])):
+	_TORTOISEPROCPATH = settings['path']
+
+else:
+	_TORTOISEPROCPATH = _TORTOISEPROCPATHDEFAULT
+
+
+
+class SVNCommit(DirectoryPaneCommand):
+	"""docstring for SVNCommit"""
+	def __call__(self):
+		url = self.pane.get_path()
+		scheme, path = splitscheme(url)
+
+		paths=[]
+		paths.append(as_url(path))	
+		if scheme != 'file://':
+			show_alert('{} is not supported'.format(url))
+			return
+
+		openCommand(" /command:commit /path:", paths, path)	
+
+
+
+
+
+class TortoiseSVNProcSetPath(DirectoryPaneCommand):
+	def __call__(self):
+		if set_tortoisesvnproc_install_path():
+			show_alert('TortoiseSVNProc.exe path updated')
+		else:
+			show_alert('Failed to update TortoiseSVNProc.exe path')
+
+def to_path(url):
+	return splitscheme(url)[1]
+
+def set_tortoisesvnproc_install_path():
+	new_tortoisesvnproc_filepath, ok = show_prompt('Enter full path to TortoiseSVNProc.exe program here', default = get_current_tortoisesvnproc_install_path(), selection_start = 0, selection_end = None )
+
+	if not ok:
+		return False
+
+	if not exists(as_url(new_tortoisesvnproc_filepath)):
+		show_alert('Path to TortoiseSVNProc given is invalid')
+		return False
+
+	_TORTOISEPROCPATH = new_tortoisesvnproc_filepath
+	save_json(_TORTOISEPROCCONFIGFILE, {'path': new_tortoisesvnproc_filepath})
+	return True
+
+def get_current_tortoisesvnproc_install_path():
+	settings = load_json(_TORTOISEPROCCONFIGFILE, default={'path': _TORTOISEPROCPATHDEFAULT})
+
+	if settings['path'] and exists(as_url(settings['path'])):
+		return settings['path']
+	else:
+		return _TORTOISEPROCPATHDEFAULT
+
+def openCommand(option, files, path):
+	tortoisesvnproc_path = get_current_tortoisesvnproc_install_path()
+
+	if not exists(as_url(tortoisesvnproc_path)):
+		show_alert('Invalid TortoiseSVNProc.exe path: ' + tortoisesvnproc_path)
+		choice = show_alert('Update Path to TortoiseSVNProc.exe?', buttons = YES | NO )
+
+		if choice == YES:
+			if not set_tortoisesvnproc_install_path():
+				# user failed to set sublime install path. bail.
+				show_alert('command failed because no valid path to TortoiseSVNProc.exe given')
+				return
+
+		else:
+			# no path to use, user doesnt want to set one now. bail.
+			show_alert('command failed because no valid path to TortoiseSVNProc.exe given')
+			return
+
+	# _TORTOISEPROCPATH = as_human_readable('file://'+ tortoisesvnproc_path)
+	_TORTOISEPROCPATH =  tortoisesvnproc_path
+
+	# TODO: Check if quoting is working for other platforms
+	args = [shlex.quote(to_path(x)) for x in files]
+	cmd= _TORTOISEPROCPATH  + " " + option + " ".join(args)
+
+	f= open('c:\\tmp\\svn.txt', 'a')
+	f.write('\nCommand:' + cmd)
+	f.close()	
+	
+	env = create_clean_environment()
+	Popen(cmd, shell=False, cwd=path,
+		stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL, env=env)
+
+def create_clean_environment():
+	# Pyinstaller, used by fman to ship on Linux sets LD_LIBRARY_PATH, which
+	# prevents starting Qt5 applications. Remove the variable if it is set.
+	env = dict(os.environ)
+	try:
+		del env['LD_LIBRARY_PATH']
+	except KeyError:
+		pass
+	return env		
